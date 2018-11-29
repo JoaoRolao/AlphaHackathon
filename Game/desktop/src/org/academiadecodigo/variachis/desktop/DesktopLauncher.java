5d788bca49a6158d76e34f566e6478299b035435
Joao Rolao <joao18089@gmail.com>
2018-11-29 13:35:45 +0000

    added libgdx

---
+++ Game/desktop/src/org/academiadecodigo/variachis/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.variachis.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.variachis.Alpha;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Alpha(), config);
+	}
+}
